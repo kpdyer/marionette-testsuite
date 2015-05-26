bcf2f045a5ca92565a8168ad6e80aaa943a125af
Kevin P. Dyer <kpdyer@gmail.com>
2015-05-26 15:21:37 -0700

    fix for nmap scan

--- marionette-testsuite/http/nmap_tests.py
+++ marionette-testsuite/http/nmap_tests.py
@@ -25,22 +25,20 @@ class Tests(unittest.TestCase):
         execute("pkill -9 -f marionette_server")
 
     def test_active_probing1(self):
-            try:
-                self.startservers("nmap/kpdyer.com")
-                remote_host = '127.0.0.1'
-                remote_port = '8080'
+        try:
+            self.startservers("nmap/kpdyer.com")
+            remote_host = '127.0.0.1'
+            remote_port = '8080'
 
-                nm = nmap.PortScanner()
-                nm.scan(remote_host, remote_port)
-                tcp_obj = nm[remote_host].tcp(int(remote_port))
+            nm = nmap.PortScanner()
+            nm.scan(remote_host, remote_port)
+            tcp_obj = nm[remote_host].tcp(int(remote_port))
 
-                self.assertEqual(tcp_obj['name'],    'http')
-                self.assertEqual(tcp_obj['product'], 'Apache httpd')
-                self.assertEqual(tcp_obj['version'], '2.4.7')
-                self.assertEqual(tcp_obj['cpe'],     'cpe:/a:apache:http_server:2.4.7')
-
-            finally:
-                self.stopservers()
+            self.assertEqual(tcp_obj['name'],    'http')
+            self.assertEqual(tcp_obj['product'], 'Apache httpd')
+            self.assertEqual(tcp_obj['version'], '2.4.7')
+        finally:
+            self.stopservers()
 
 if __name__ == '__main__':
     unittest.main()
