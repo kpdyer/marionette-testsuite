f20bbdf048a2bf5b1e0676d512ac273d9ea261ef
Kevin P. Dyer <kpdyer@gmail.com>
2015-05-26 19:04:40 -0700

    fix for broken unit tests

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 
 from setuptools import setup
 
-setup(name='marionette-testsuite',
-      test_suite='marionette-testsuite',
-      version='alpha',
+setup(name='marionette_testsuite',
+      test_suite='marionette_testsuite',
+      packages=['marionette_testsuite','marionette_testsuite.http'],
+      version='0.0.1',
       description='Marionette Test Suite',
       author='Kevin P. Dyer',
       author_email='kpdyer@gmail.com',
-      url='https://github.com/kpdyer/marionette-testsuite')
+      url='https://github.com/kpdyer/marionette_testsuite')
