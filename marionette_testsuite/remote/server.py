3b0726681b7905c3b853514f7aff17ed8434029e
Jack Pickett <jack.pickett@redjack.com>
2015-07-28 21:11:12 -0400

    Converted server.py to subprocess

--- marionette_testsuite/remote/server.py
+++ marionette_testsuite/remote/server.py
@@ -7,24 +7,22 @@ import unittest
 import time
 
 import argparse
-import marionette_tg.conf
-
-def execute(cmd):
-    subprocess.Popen(["nohup", "python", cmd])
 
 def startservers(format):
-    proxy_ip = marionette_tg.conf.get("server.proxy_ip")
-    server_ip = marionette_tg.conf.get("server.server_ip")
-    httpserver = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                              "httpserver --local_port 18081 &")
-    execute(httpserver)
-    execute("marionette_server  --proxy_ip %s --proxy_port 18081 --server_ip %s --format %s &" %
-            (proxy_ip, server_ip, format))
+    httpserver = [os.path.join(os.path.dirname(os.path.realpath(__file__)),"httpserver"),
+                  "--local_port", "18081"]
+    subprocess.Popen(httpserver)
     time.sleep(1)
+    marionetteserver = ["marionette_server",
+                       "--proxy_ip", "127.0.0.1",
+                       "--proxy_port", "18081",
+                       "--server_ip", "0.0.0.0",
+                       "--format", format]
+    subprocess.Popen(marionetteserver)
 
 def stopservers():
-    execute("pkill -9 -f marionette_server")
-    execute("pkill -9 -f httpserver")
+    subprocess.Popen(["pkill", "-9", "-f", "marionette_server"])
+    subprocess.Popen(["pkill", "-9", "-f", "httpserver"])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
